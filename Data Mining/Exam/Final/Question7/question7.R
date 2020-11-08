67de460187895053dc479146fff73cbb5e3fe391
Sri Balaji <55683994+Balaji751@users.noreply.github.com>
2020-11-08 11:57:57 +0530

    Exam solutions

---
+++ Data Mining/Exam/Final/Question7/question7.R
@@ -0,0 +1,18 @@
+getwd()
+setwd("C:/DataScience_2019501101/DataMining/Exam/Final")
+
+
+data = read.csv("Live_data.csv", header = FALSE, col.names = c("1", "2", "3", "4", "5", "6", "7"))
+
+
+str(data)
+summary(data)
+
+
+x = data[,1:5]
+y = data[,6]
+fit = kmeans(x,4)
+library(class)
+knnfit = knn(fit$centers,x,as.factor(c(-2,-1,1,2)))
+error = 1-sum(knnfit == y)/length(y)
+error
